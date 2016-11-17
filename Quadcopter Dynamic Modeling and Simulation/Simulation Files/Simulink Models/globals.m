66a9cfbe2c491f7284f0a5f4d4873d052317f181
abhishekrajdutta <abhishek.eed.svnit@gmail.com>
2016-11-18 01:14:46 +0530

    nonlinear controller

---
+++ Quadcopter Dynamic Modeling and Simulation/Simulation Files/Simulink Models/globals.m
@@ -0,0 +1,38 @@
+%This script initialises global values for the simulation
+
+global zeta;%zeta value for the imu
+zeta=0.707;
+
+global wn%natural frequency of IMU
+wn=1000;
+
+global bias;%sensor bias
+bias=0.03;
+
+global freq;%Frequency at which the sensor samples
+freq=200;
+
+global noisea;%accelerometer noise value
+noisea=4.84e-8;
+
+global kpp;
+kpp=2;
+
+global kip;
+kip=1.2;
+
+global kdp;
+kdp=1.5;
+
+global kpr;
+kpr=2;
+
+global kir;
+kir=1.2;
+
+global kdr;
+kdr=1.5;
+
+
+
+
